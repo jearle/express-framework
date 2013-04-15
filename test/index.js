15c6ff96370dc42ff5c7a6fa8121bc8519196742
Jesse Earle <jearle@Jesses-MacBook-Air.local>
2013-04-15 19:48:12 -0400

    added more tests to mongo

--- test/index.js
+++ test/index.js
@@ -1,5 +1,8 @@
 var should = require('should')
-  , lib = require('../lib');
+  , lib = require('../lib')
+  , express = require('express')
+  , http = require('http')
+  , app = express();
 
 describe('lib', function(){
   
@@ -32,7 +35,7 @@ describe('lib', function(){
     });
   });
 
-  describe('getArgStringsFromFunction', function(){
+  describe('#getArgStringsFromFunction()', function(){
     it('should return an array with the correct arg name and length (1 arg)', function(){
       var func = function Func($arg1){};
 
@@ -46,6 +49,98 @@ describe('lib', function(){
       lib.getArgStringsFromFunction(func).length.should.equal(2);
       lib.getArgStringsFromFunction(func).should.include('$arg1');
       lib.getArgStringsFromFunction(func).should.include('$arg2');
+    });
+  });
+
+  describe('#containsModelArg()', function(){
+
+    it('should equal true', function(){
+      var args1 = ['$model'];
+      lib.containsModelArg(args1).should.equal(true);
+    });
+    it('should equal false', function(){
+      var args2 = ['$random'];
+      lib.containsModelArg(args2).should.equal(false);
+    });
+    
+  });
+
+  describe('#containsRouteArg()', function(){
+
+    it('should equal true', function(){
+      var args1 = ['$route'];
+      lib.containsRouteArg(args1).should.equal(true);
+    });
+    it('should equal false', function(){
+      var args2 = ['$random'];
+      lib.containsRouteArg(args2).should.equal(false);
+    });
+    
+  });
+
+  describe('#setAppRoutes()', function(){
+    it('should contain a get route of "/" pointing to the same callback', function(done){
+      lib.set('app', app);
+      var mockRoutes = {
+        get: {
+          '/': function(req, res){}
+        },
+        put: {},
+        post: {},
+        delete: {}
+      };
+      lib.setAppRoutes('/base', mockRoutes);
+      var server = http.createServer(lib.get('app')).listen(9999, function(){
+        lib.get('app').routes.get[0].path.should.equal('/');
+        lib.get('app').routes.get[0].callbacks[0].should.equal(mockRoutes.get['/']);
+        lib.get('app').routes.get.pop();
+        server.close();
+        done();
+      });
+    });
+
+    it('should contain a get route of "/test" pointing to the same callback', function(done){
+      lib.set('app', app);
+      var mockRoutes = {
+        get: {
+          '/': function(req, res){}
+        },
+        put: {},
+        post: {},
+        delete: {}
+      };
+      lib.setAppRoutes('/test', mockRoutes);
+      var server = http.createServer(lib.get('app')).listen(9999, function(){
+        lib.get('app').routes.get[0].path.should.equal('/test');
+        lib.get('app').routes.get[0].callbacks[0].should.equal(mockRoutes.get['/']);
+        server.close();
+        done();
+      });
+    });
+  });
+
+  describe('#injectModel()', function(){
+    var $model = {schema:{}, static:{}, method:{}};
+    var testModel = function TestModel($model){
+      $model.schema = {
+        testField1: String,
+        testField2: String
+      };
+      $model.static = {
+        staticMethod: function(){return 'test_static';}
+      };
+      $model.method = {
+        methodMethod: function(){return 'test_method'}
+      };
+    };
+
+    it('should create a model named TestModel on lib.models', function(){
+      lib.injectModel(testModel);
+      should.exist(lib.models.TestModel);
+    });
+
+    it('it should be a mongo object', function(){
+      new lib.models.TestModel().should.have.property('_id');
     });
   });
 });
